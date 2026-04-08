8082b33ac7bf9d4150a7c33ed65e86a77e79acfe
asimkumarjaiswal <asim.101043@gmail.com>
2026-04-08 20:17:31 +0530

    AI vision and Language Testing with Azure Ai services

--- AI102.Infrastructure/Services/VisionService.cs
+++ AI102.Infrastructure/Services/VisionService.cs
@@ -21,23 +21,29 @@ public class VisionService : IVisionService
         if (string.IsNullOrWhiteSpace(imageUrl))
             throw new Exception("Image URL is required.");
 
-        var client = new ImageAnalysisClient(new Uri(_options.Endpoint),new AzureKeyCredential(_options.Key));
-
-        var result = await client.AnalyzeAsync(new Uri(imageUrl),VisualFeatures.Caption | VisualFeatures.Tags);
-
-        var response = new ImageAnalysisResponseDto();
-
-        if (result.Value.Caption != null)
+        var client = new ImageAnalysisClient(new Uri(_options.Endpoint), new AzureKeyCredential(_options.Key));
+        try
         {
-            response.Caption = result.Value.Caption.Text;
+            var result = await client.AnalyzeAsync(new Uri(imageUrl), VisualFeatures.Caption | VisualFeatures.Tags);
+            var response = new ImageAnalysisResponseDto();
+
+            if (result.Value.Caption != null)
+            {
+                response.Caption = result.Value.Caption.Text;
+            }
+
+            if (result.Value.Tags != null)
+            {
+                response.Tags = result.Value.Tags.Values.Select(t => t.Name).ToList()!;
+            }
+            return response;
+        }
+        catch (RequestFailedException ex)
+        {
+
+            throw new Exception(ex.Message);
         }
 
-        if (result.Value.Tags != null)
-        {
-            response.Tags = result.Value.Tags.Values.Select(t => t.Name).ToList()!;
-        }
-
-        return response;
     }
 
     public async Task<ReadTextResponseDto> ReadTextAsync(string imageUrl)
